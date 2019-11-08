e36251a5dc911b31523c966bf15c5177d3d0db31
itcast-hsian <541510140@qq.com>
2019-11-08 17:38:44 +0800

    添加首页轮播图

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,16 +5,20 @@
 
     <!-- router-view: 显示路由匹配到组件, nuxt组件就是router-view -->
     <nuxt />
+
+    <!-- 公共页脚组件 -->
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from "@/components/header"
-
+import Footer from "@/components/footer"
 export default {
   // 注册组件
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,61 @@
 <template>
-  <div>首页</div>
+    <div class="container">
+
+        <!-- 幻灯片 -->
+        <!-- interval是轮播图切换的间隔时间 -->
+        <!-- arrow是否显示箭头 -->
+        <el-carousel 
+        :interval="5000" 
+        arrow="always">
+            <el-carousel-item 
+            v-for="(item, index) in banners" 
+            :key="index">
+                <div class="banner-image" 
+                :style="`
+                background:url(${$axios.defaults.baseURL + item.url}) center center no-repeat;
+                background-size:contain contain;
+                `">
+                </div>
+            </el-carousel-item>
+        </el-carousel>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            // 轮播图数据
+            banners: []
+        }
+    },
+    mounted(){
+      // 请求接口数据
+      this.$axios({
+        url: "/scenics/banners"
+      }).then(res => {
+        // data是轮播图的数组
+        const {data} = res.data;
+        // 赋值data的banners
+        this.banners = data;
+      })
+    }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
 
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
 </style>
