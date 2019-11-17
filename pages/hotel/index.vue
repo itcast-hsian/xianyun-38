c2c162b037ec654042081449c6bf372f4817ae37
itcast-hsian <541510140@qq.com>
2019-11-17 15:19:55 +0800

    在页面中显示地图

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>酒店首页</div>
+  <div>
+    <h1>高德地图</h1>
+    <div id="container"></div> 
+  </div>
 </template>
 
 <script>
 export default {
+  mounted(){
+    window.onLoad  = function(){
+        var map = new AMap.Map('container', {
+          zoom:11,//级别
+          center: [113.3245904, 23.1066805],//中心点坐标
+      });
+    }
 
+    var url = 'https://webapi.amap.com/maps?v=1.4.15&key=20918d40bbe868d23b3a148d9b8aebd2&callback=onLoad';
+    var jsapi = document.createElement('script');
+    jsapi.charset = 'utf-8';
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+#container {width:500px; height: 500px; }  
 </style>
